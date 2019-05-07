5bf1be6423dce6762849b7361cc744f923b711d6
Mateusz Weigle <matiwei@gmail.com>
2019-05-07 20:42:25 +0200

    added solving full lines to list of strings easy to extend by spaces

---
+++ monogramsSolver.hs
@@ -0,0 +1,30 @@
+import Data.Char
+import Debug.Observe
+import Debug.Trace
+
+lineTest1 = [(1,'*'),(2,'x'),(3,'x'),(2,'*')];
+lineTest2 = [(1,'*'),(2,'*'),(3,'*'),(2,'*')];
+
+solveLine  (size, xs)
+                    |(lineLengthSum(xs)==size) = line(xs)
+                    |(lineLengthSum(xs)>size) = error("Too long")
+                    
+insertIntoSimple :: (Int, [[Char]]) -> [[Char]]
+insertIntoSimple (_,[]) = [];
+insertIntoSimple (0,xs) = xs;
+
+lineSolver [] = [];
+lineSolver (xs) = [""]++line(xs)++[""]
+
+line :: [(Integer,Char)] -> [[Char]]
+--line (xs) | trace ("line " ++ show xs) False = undefined
+line [] = [""];
+line [x] = [lineHelper(fst x, snd x)];
+line (x:y:xs) = line [x] ++ colorEqInsert2((snd x),(snd y)) ++ line (y:xs);
+
+colorEqInsert2 (a,b) = if (a==b) then [" "] else [""]
+
+--generates strings with length of first arg from char form second
+lineHelper :: (Integer,Char) -> [Char]
+lineHelper (0,_) = ""
+lineHelper (count,letter) = letter : lineHelper(count-1, letter)
