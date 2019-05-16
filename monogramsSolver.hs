932ac2f37bb8e5bf08df643c5b7fb0da6eb268fa
mchrost <marchrostowski@gmail.com>
2019-05-16 20:20:29 +0200

    Connected line pattern with all possible space combinations

--- monogramsSolver.hs
+++ monogramsSolver.hs
@@ -3,7 +3,9 @@ import Data.Char
 import Data.List 
 import Debug.Trace
 
+lineTest1 :: [(Int, Char)]
 lineTest1 = [(1,'*'),(2,'x'),(3,'x'),(2,'*')];
+lineTest2 :: [(Int, Char)]
 lineTest2 = [(1,'*'),(2,'*'),(3,'*'),(2,'*')];
 
 solveLine  (size, xs)
@@ -19,7 +21,7 @@ insertIntoSimple (0,xs) = xs;
 lineSolver [] = [];
 lineSolver (xs) = [""]++line(xs)++[""]
 
-line :: [(Integer,Char)] -> [[Char]]
+line :: [(Int,Char)] -> [[Char]]
 --line (xs) | trace ("line " ++ show xs) False = undefined
 line [] = [""];
 line [x] = [lineHelper(fst x, snd x)];
@@ -28,7 +30,7 @@ line (x:y:xs) = line [x] ++ colorEqInsert2((snd x),(snd y)) ++ line (y:xs);
 colorEqInsert2 (a,b) = if (a==b) then [" "] else [""]
 
 --generates strings with length of first arg from char from second
-lineHelper :: (Integer,Char) -> [Char]
+lineHelper :: (Int,Char) -> [Char]
 lineHelper (0,_) = ""
 lineHelper (count,letter) = letter : lineHelper(count-1, letter)
 
@@ -41,12 +43,12 @@ colorEq (a,b) = if (a==b) then 1 else 0
 --creates tree with all space combinations
 data Tree a = Empty | Node a [Tree a]
 
-generateNodes :: Integer -> Integer -> [Tree [Char]]
+generateNodes :: Int -> Int -> [Tree [Char]]
 generateNodes 0 _ = [Empty]
 generateNodes 1 m = [Node (addSpaces m []) [Empty]]
 generateNodes n m = [Node (addSpaces k []) (generateNodes (n-1) (m-k)) | k <- [0..m]] 
  
-addSpaces :: Integer -> [Char] -> [Char]
+addSpaces :: Int -> [Char] -> [Char]
 addSpaces 0 x = ""
 addSpaces m x = x ++ " " ++ addSpaces (m - 1) x    
 
@@ -57,3 +59,32 @@ generateSpaceCombinations [Empty] [] = []
 generateSpaceCombinations [Empty] z = z
 generateSpaceCombinations ((Node a b):c) [] = if length c == 0 then generateSpaceCombinations b [[a]] else generateSpaceCombinations b [[a]] ++ generateSpaceCombinations c []
 generateSpaceCombinations ((Node a b):c) [z] = if length c == 0 then generateSpaceCombinations b [(z ++ [a])] else generateSpaceCombinations b [(z ++ [a])] ++ generateSpaceCombinations c [z]
+
+createSpaceList :: Int -> Int -> [[[Char]]]
+createSpaceList a b = generateSpaceCombinations (generateNodes a b) []
+
+--create all line or column combinations
+generateAllLineCombinations :: [[Char]] -> [[[Char]]]-> [[[Char]]]
+generateAllLineCombinations x [] = []
+generateAllLineCombinations x (y:ys) = [combineLineWithSpaces x y 0] ++ generateAllLineCombinations x ys
+
+combineLineWithSpaces :: [[Char]] -> [[Char]] -> Int -> [[Char]]
+combineLineWithSpaces [] _ _ = []
+combineLineWithSpaces (x:xs) (y:ys) z   | odd z = [addSpacesToEvenIndexes x y z] ++ combineLineWithSpaces xs ys (z + 1)
+                                        | otherwise = [addSpacesToEvenIndexes x y z] ++ combineLineWithSpaces xs (y:ys) (z + 1)       
+
+addSpacesToEvenIndexes :: [Char] -> [Char] -> Int -> [Char]
+addSpacesToEvenIndexes x y a    | odd a = x 
+                                | otherwise = x ++ y
+
+computeInterspacesToFill :: [[Char]] -> Int
+computeInterspacesToFill a = div (length a - 1) 2 + 1 
+
+generateLineAndSpaces :: [(Int,Char)] -> Int -> [[[Char]]]
+generateLineAndSpaces [] _ = []
+generateLineAndSpaces pattern size = generateAllLineCombinations a (createSpaceList (computeInterspacesToFill a) b)
+    where   a = lineSolver pattern
+            b = size - (lineLengthSum pattern)
+            
+
+--Instruction: Use "generateLineAndSpaces pattern size" to generate all possible combinations of line and spaces
